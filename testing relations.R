1a35a09ddad493b08ae4911087ccefa3faada6d4
Andrew Mc Daid <andrewmcdaid97@gmail.com>
2021-05-04 14:33:22 +0100

    Beaver Temp First Commit

---
+++ testing relations.R
@@ -0,0 +1,20 @@
+# use stat methos to examine
+# relationships between vars of interest
+
+# Beavers dataset
+# contains body temp of 2 beavers captured every 10 mins
+# over 24 hours
+# we want to examine difference in average body temp
+# to evaluate wheter body temp is affected by activity
+
+# h0: mean temperature is not affected by activity
+# h1: mean temp is affected by activity
+
+
+str(beaver2)
+
+
+# evaluate beaver temp and activity
+
+# temp = continuous variable
+# activity = categoriacal dicotomous variable
